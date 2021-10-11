139cd4b96cfb46d35e28f816fcadd188c1f2285f
Jithin <jithin@techis.io>
2021-10-11 13:27:31 +0530

    added files

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -153,4 +153,9 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+
